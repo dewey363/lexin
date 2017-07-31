a46f6b19c9061d677c8928cf057588efe256c775
WangJian <63928427@qq.com>
2017-07-31 14:10:25 +0800

    no message

--- data/conf/db.php
+++ data/conf/db.php
@@ -7,7 +7,7 @@ return array(
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'crms',
     'DB_USER' => 'root',
-    'DB_PWD' => 'root',
+    'DB_PWD' => 'XsKKiMUQ',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'yl_',
     //密钥
