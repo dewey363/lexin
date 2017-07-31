e13795403a27d863227b086ba5387d793fcdeb51
WangJian <63928427@qq.com>
2017-07-31 14:16:25 +0800

    no message

--- application/Index/Controller/IndexController.class.php
+++ application/Index/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Index\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        header("location: http://crms.dev/admin");
+        header("location: http://111.231.63.219/admin");
     }
 }
