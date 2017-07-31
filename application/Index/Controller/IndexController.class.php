4c948d7f6214dae2400a7540d6f5507a1925b26b
WangJian <63928427@qq.com>
2017-07-31 14:19:00 +0800

    no message

--- application/Index/Controller/IndexController.class.php
+++ application/Index/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Index\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        header("location: http://111.231.63.219/admin");
+        header("location: http://111.231.63.219:8080/admin");
     }
 }
