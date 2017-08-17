e145546988549ca895947d85e5023900233dc7f6
WangJian <63928427@qq.com>
2017-08-17 17:50:21 +0800

    no message

--- application/Index/Controller/IndexController.class.php
+++ application/Index/Controller/IndexController.class.php
@@ -4,6 +4,6 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
 //        header("location: http://crms.dev/admin");
-        header("location: http://crm.yh-360.com/admin");
+        header("location: http://111.231.63.219/admin");
     }
 }
